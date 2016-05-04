bac7fd2f1b73070e9128b5f68f315405ad13612a
Sean Robertson <sprobertson@gmail.com>
2016-05-04 16:42:23 -0700

    service subscriptions and publishing

--- service.lua
+++ service.lua
@@ -1,6 +1,5 @@
 local zmq = require 'lzmq'
 local zloop = require 'lzmq.loop'
-local zthreads = require 'lzmq.threads'
 local cjson = require 'cjson'
 
 local helpers = require './helpers'
@@ -20,11 +19,12 @@ function Service.create(name, methods)
     service.name = name
     service.port = math.random(5000, 35000)
     service.methods = methods
+    service.subscriptions = {}
 
     service.ctx = zmq.context()
     service.loop = zloop.new(1, service.ctx)
     service.registry_connection = Connection.create(service.ctx, service.loop, "tcp://localhost:8420")
-    service.binding = Binding.create(service.ctx, service.loop, service.port, function(client_id, message_id, method, args) service:handleMethod(client_id, message_id, method, args) end)
+    service.binding = Binding.create(service.ctx, service.loop, service.port, function(client_id, message) service:handleMessage(client_id, message) end)
 
     service.loop:add_once(heartbeat_interval, function() service:sendPing() end)
 
@@ -36,8 +36,9 @@ function Service:register(cb)
         id=self.id,
         name=self.name,
         port=self.port,
-        heartbeat=heartbeat_interval
+        heartbeat=0
     }
+        -- heartbeat=heartbeat_interval
     self.registry_connection:sendMethod('registerService', {registration}, cb)
     self.loop:start()
 end
@@ -46,6 +47,15 @@ function Service:sendPing()
     self.registry_connection:sendMessage({kind='ping'}, function()
         self.loop:add_once(heartbeat_interval, function() self:sendPing() end)
     end)
+end
+
+function Service:handleMessage(client_id, message)
+    print('[handleMessage]', client_id, message)
+    if message.kind == 'method' then
+        self:handleMethod(client_id, message.id, message.method, message.args)
+    elseif message.kind == 'subscribe' then
+        self:handleSubscribe(client_id, message.id, message.type)
+    end
 end
 
 function Service:handleMethod(client_id, message_id, method, args)
@@ -59,7 +69,30 @@ function Service:handleMethod(client_id, message_id, method, args)
         self.binding.socket:sendx(client_id, response_json)
     end
     table.insert(args, cb)
+    print('[handleMethod]', method, args)
     self.methods[method](unpack(args))
+end
+
+function Service:handleSubscribe(client_id, message_id, event_type)
+    print("subscribing to", event_type)
+    if self.subscriptions[event_type] == nil then
+        self.subscriptions[event_type] = {}
+    end
+    table.insert(self.subscriptions[event_type], {client_id, message_id})
+end
+
+function Service:publish(event_type, data)
+    for i, subscription in pairs(self.subscriptions[event_type]) do
+        local client_id = subscription[1]
+        local message_id = subscription[2]
+        local event = {
+            id=message_id,
+            kind='event',
+            event=data
+        }
+        local event_json = cjson.encode(event)
+        self.binding.socket:sendx(client_id, event_json)
+    end
 end
 
 return Service
